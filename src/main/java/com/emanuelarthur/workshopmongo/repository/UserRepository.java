9b98525b7978342e7424fdb59329fccdfdea9d5b
Arthur Emanuel <arthuremanuel091@gmail.com>
2025-11-28 13:49:56 -0300

    Conectado ao Mongo com repository e service

---
+++ src/main/java/com/emanuelarthur/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.emanuelarthur.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.emanuelarthur.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String>{
+
+}
