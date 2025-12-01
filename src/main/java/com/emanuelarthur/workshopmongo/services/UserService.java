29bc9af475ce86999dc2d215d45fd98b67541be5
Arthur Emanuel <arthuremanuel091@gmail.com>
2025-12-01 20:37:23 -0300

    Inserção de User com Post

--- src/main/java/com/emanuelarthur/workshopmongo/services/UserService.java
+++ src/main/java/com/emanuelarthur/workshopmongo/services/UserService.java
@@ -7,20 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.emanuelarthur.workshopmongo.domain.User;
+import com.emanuelarthur.workshopmongo.dto.UserDTO;
 import com.emanuelarthur.workshopmongo.repository.UserRepository;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository repo;
+	@Autowired
+	private UserRepository repo;
 
-    public List<User> findAll() {
-        return repo.findAll();
-    }
+	public List<User> findAll() {
+		return repo.findAll();
+	}
 
-    public User findById(String id) {
-        return repo.findById(id)
-                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-    }
+	public User findById(String id) {
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
